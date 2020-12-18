1e8f3454f9f855f458e6e603a98eed1b6763727c
LeakedMemory <elmq0022@umn.edu>
2020-12-18 16:08:38 -0600

    adds formatted published_date

--- apps/blog/serializers.py
+++ apps/blog/serializers.py
@@ -1,14 +1,18 @@
 from .models import Article, Tag
-from rest_framework import routers, serializers, viewsets
+from rest_framework import serializers
 
 
 class ArticleSerializer(serializers.ModelSerializer):
+
+    published_date = serializers.DateTimeField(format="%Y-%m-%d at %I:%M%p")
+
     class Meta:
         model = Article
         fields = [
-            "title",
-            "post",
             "pk",
+            "post",
+            "published_date",
+            "title",
         ]
 
 
