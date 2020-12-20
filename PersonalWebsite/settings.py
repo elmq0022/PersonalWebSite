04c978c7725f55975633fdd7c29d4e7c03de5879
LeakedMemory <elmq0022@umn.edu>
2020-12-20 09:20:43 -0600

    removes homepage and about applications

--- PersonalWebsite/settings.py
+++ PersonalWebsite/settings.py
@@ -46,9 +46,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'tinymce',
     'corsheaders',
-    # project
-    'apps.homepage',
-    'apps.about',
+    # apps
     'apps.blog',
 ]
 
--- PersonalWebsite/urls.py
+++ PersonalWebsite/urls.py
@@ -2,8 +2,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('', include('apps.homepage.urls')),
-    path('about/', include('apps.about.urls')),
     path('blog/', include('apps.blog.urls')),
     path('admin/', admin.site.urls),
 
